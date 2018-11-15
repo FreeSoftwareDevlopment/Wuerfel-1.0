14109090594c1d59794d05c9c5d5de815b4981bb
admin <noreply>
2018-11-15 17:14:42 +0100

    index

---
+++ noopen.py
@@ -0,0 +1,7 @@
+import time
+import random
+import sys
+Wuerfel = ['1','2','3','4','5','6']
+time.sleep(0.5)
+ergebnis = random.choice(Wuerfel)
+print (ergebnis)
---
+++ run.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+// cl /EHsc /DllMain run.cpp
+int main(){
+	int var = 1;
+	int cooseing;
+	system("title Wuerfel");
+	while (var < 12) {
+		cout << "Die Software hat gewuerfelt, und das ergebnis ist:" << endl;
+		system("python noopen.py");
+		cout << endl << endl;
+		cout << "Nochmal Wuerfeln? 1 fuer Ja, und 0 fuer nein." << endl;
+		cin >> cooseing;
+		switch (cooseing) {
+		case 0:
+			cerr << "Software beendet" << endl;
+			while (var < 13) {
+				++var;
+			}
+			break;
+		case 1:
+			cout << endl << endl << "Wuerfelt" << endl;
+		}
+
+	}
+}
